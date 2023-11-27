dd353ca010652f25d6040d75514d33cdcf4bbd09
pjw <bananasyndro@naver.com>
2023-11-27 18:20:46 +0900

    2023-11-27

--- javaEx/src/io/Repeat.java
+++ javaEx/src/io/Repeat.java
@@ -2,69 +2,49 @@ package io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.time.LocalDate;
-import java.util.Date;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 public class Repeat {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		File file = new File("src/io/text3");
-		file.createNewFile();
-		if (file.exists()) {
-			Writer wr = new FileWriter(file);
-			wr.write("Apple\nBanana\nCoconut\nWatermelon");
-			wr.flush();
-			wr.close();
-			System.out.println("파일쓰기완료");
+		String url_path = "https://www.google.com";
+		URL url = new URL(url_path);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setUseCaches(false);
+		conn.setReadTimeout(20000);
+		conn.setRequestMethod("GET");
+
+		// 데이터 처리
+		if (conn.getResponseCode() == 200) {
+			StringBuffer sb = new StringBuffer();
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				sb.append(line + "\n");
+				br.close();
+				String Result = sb.toString();
+			}
 		}
+		conn.disconnect();
 
-		LocalDate date = LocalDate.now();
-		File file2 = new File("src/io/text_" + date + ".txt");
-		new File("src/io/").mkdir();
-		file2.createNewFile();
+		Path path = Paths.get("src/io/mytext.txt");
+		BufferedWriter bw = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
 
-		InputStream is = new FileInputStream(file);
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2)));
-
-		int cnt;
-		
-		while ((cnt = is.read()) != -1) {
-			bw.write(cnt);
-		}
-		bw.flush();
-		bw.close();
-		is.close();
-		
-		
-		
-//		while ((cnt = br.read()) != -1) {
-//			bw.write(cnt);
-//		}
-//		bw.flush();
-//		bw.close();
-//		br.close();
-		
-		BufferedReader br2 = new BufferedReader(new InputStreamReader(new FileInputStream(file2)));
-		StringBuffer sb = new StringBuffer();
-		String line;
-		while ((line = br2.readLine()) != null) {
-			sb.append(line+"\n");
-
-		}
-		br2.close();
-		System.out.println(sb);
 	}
 
 }
